e1881e207bc789ea6c3bc04421acc3362d27b2f4
allanmessias <allanmessiasdecoelho@gmail.com>
2022-05-09 14:46:09 -0300

    added crud funcionality

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,12 +4,80 @@ class UserController {
     async store(req, res) {
         try {
             const novoUser = await User.create(req.body);
-            res.json(novoUser);
+            return res.json(novoUser);
         } catch (e) {
             const error = {
                 errs: e.errors.map((err) => err.message),
             };
-            res.status(400).json(error);
+            return res.status(400).json(error);
+        }
+    }
+
+    async index(req, res) {
+        try {
+            const users = await User.findAll();
+            return res.json(users);
+        } catch (e) {
+            return res.json(null);
+        }
+    }
+
+    async show(req, res) {
+        try {
+            const user = await User.findByPk(req.params.id);
+            return res.json(user);
+        } catch (e) {
+            return res.json(null);
+        }
+    }
+
+    async update(req, res) {
+        try {
+            /**
+             * Condição não funcionando, checar depois
+             * if (!req.params.id) {
+                return res.status(400).json({
+                    errors: ['ID NÃO ENVIADO'],
+                });
+            } */
+            const user = await User.findByPk(req.params.id);
+
+            if (!user) {
+                return res.status(400).json({
+                    errors: ['Usuário não existe'],
+                });
+            }
+            const updatedUser = await user.update(req.body);
+
+            return res.json(updatedUser);
+        } catch (e) {
+            const err = {
+                errs: e.errors.map((err) => err.message),
+            };
+            return res.status(400).json(err);
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            if (!req.params.id) {
+                return res.status(400).json({
+                    errors: ['ID NÃO ENVIADO'],
+                });
+            }
+
+            const user = await User.findByPk(req.params.id);
+
+            if (!user) {
+                return res.status(400).json({
+                    errors: ['Usuário não existe'],
+                });
+            }
+
+            const deletedUser = await user.destroy();
+            return res.json(deletedUser);
+        } catch (e) {
+            return res.json(null);
         }
     }
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,6 +5,10 @@ const router = new Router();
 
 // Cria novo usário
 router.post('/', UserController.store);
+router.get('/', UserController.index);
+router.get('/:id', UserController.show);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export default router;
 
