4dde0cd28e754fd3a3d867bf51cd80d85a32364b
allanmessias <allanmessiasdecoelho@gmail.com>
2022-05-09 12:46:02 -0300

    resolving issues to create user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,17 +3,13 @@ import User from '../models/User';
 class UserController {
     async store(req, res) {
         try {
-            const novoUser = await User.create({
-                nome: 'Allan',
-                email: 'allanmessiasdecoelho@gmail.com',
-                password: '123456',
-            });
+            const novoUser = await User.create(req.body);
             res.json(novoUser);
         } catch (e) {
             const error = {
                 errs: e.errors.map((err) => err.message),
             };
-            res.status('400').json(error);
+            res.status(400).json(error);
         }
     }
 }
